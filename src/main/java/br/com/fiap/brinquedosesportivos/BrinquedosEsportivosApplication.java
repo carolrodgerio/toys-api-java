785dcc59f9d407afd59e7b5dd1ccc0948fca86e7
lucas <lucaswtds@gmail.com>
2025-08-17 14:09:22 -0300

    setup: project initial structure

---
+++ src/main/java/br/com/fiap/brinquedosesportivos/BrinquedosEsportivosApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.brinquedosesportivos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrinquedosEsportivosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrinquedosEsportivosApplication.class, args);
+	}
+
+}
